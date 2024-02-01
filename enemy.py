c510746869f288859933ac3c92385136ee2231ed
dextern1atwit <90652288+dextern1atwit@users.noreply.github.com>
2024-02-01 14:39:31 -0500

    nathan fork version

--- enemy.py
+++ enemy.py
@@ -1,4 +1,7 @@
 class Enemy:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+
+  def __init__(self, name, description, hit, defeat):
+    self.name = name
+    self.description = description
+    self.defeat = defeat
+    self.hit = hit
